bff39ae0c901a05584a0450bdeae7dbb47a422ea
Tingting Zhao <RexRowan@users.noreply.github.com>
2016-08-12 09:54:27 +0100

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -29,3 +29,20 @@ mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
 header('Location: contact-form-thank-you.html');
 }
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
